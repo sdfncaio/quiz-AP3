7af775fb1ea62de793387a8c02032f8ba2d24cf1
sdfncaio <71291506+sdfncaio@users.noreply.github.com>
2020-12-03 19:02:01 -0300

    adicionei arquivos via upload

---
+++ quiz.js
@@ -0,0 +1,317 @@
+var totalAcerto = 0
+
+/*questao 1*/
+
+function acertod1(){
+    document.getElementById('d1').style.display="block"
+    if (totalAcerto){
+        totalAcerto++};
+};
+setInterval(function(){
+    document.getElementById('d1').style.display="none"
+},2000)
+
+function erroa1() {
+    document.getElementById('a1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('a1').style.display="none";
+},2000)
+
+function errob1() {
+    document.getElementById('b1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('b1').style.display="none";
+},2000)
+
+function erroc1() {
+    document.getElementById('c1').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('c1').style.display="none";
+},2000)
+
+/*questao 2 */
+
+function acertod2(acerto) {
+    document.getElementById('d2').style.display="block";
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('d2').style.display="none"
+},2000)
+
+function erroa2() {
+    document.getElementById('a2').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('a2').style.display="none";
+},2000)
+
+function errob2() {
+    document.getElementById('b2').style.display="block";
+}
+setInterval(function(){
+    document.getElementById('b2').style.display="none"
+},2000)
+
+function erroc2() {
+    document.getElementById('c2').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c2').style.display="none"
+},2000)
+
+/*questao 3 */
+
+function errod3() {
+    document.getElementById('d3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d3').style.display="none"
+},2000)
+
+function acertoa3(acerto) {
+    document.getElementById('a3').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('a3').style.display="none"
+},2000)
+
+function errob3() {
+    document.getElementById('b3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b3').style.display="none"
+},2000)
+
+function erroc3() {
+    document.getElementById('c3').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c3').style.display="none"
+},2000)
+
+/*questao 4 */
+
+function errod4() {
+    document.getElementById('d4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d4').style.display="none"
+},2000)
+
+function erroa4() {
+    document.getElementById('a4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a4').style.display="none"
+},2000)
+
+function errob4() {
+    document.getElementById('b4').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b4').style.display="none"
+},2000)
+
+function acertoc4(acerto) {
+    document.getElementById('c4').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('c4').style.display="none"
+},2000) 
+
+/*questao 5 */
+
+function errod5() {
+    document.getElementById('d5').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d5').style.display="none"
+},2000)
+
+function erroa5() {
+    document.getElementById('a5').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a5').style.display="none"
+},2000)
+
+function acertob5(acerto) {
+    document.getElementById('b5').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('b5').style.display="none"
+},2000)
+
+function arroc5() {
+    document.getElementById('c5').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c5').style.display="none"
+},2000) 
+
+/*questao 6 */
+
+function errod6() {
+    document.getElementById('d6').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d6').style.display="none"
+},2000)
+
+function acertoa6(acerto) {
+    document.getElementById('a6').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('a6').style.display="none"
+},2000)
+
+function errob6() {
+    document.getElementById('b6').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b6').style.display="none"
+},2000)
+
+function arroc6() {
+    document.getElementById('c6').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c6').style.display="none"
+},2000) 
+
+/*questao 7 */
+
+function acertod7(acerto) {
+    document.getElementById('d7').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('d7').style.display="none"
+},2000)
+
+function erroa7() {
+    document.getElementById('a7').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a7').style.display="none"
+},2000)
+
+function errob7() {
+    document.getElementById('b7').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b7').style.display="none"
+},2000)
+
+function arroc7() {
+    document.getElementById('c7').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c7').style.display="none"
+},2000) 
+
+/*questao 8 */
+
+function acertod8(acerto) {
+    document.getElementById('d8').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('d8').style.display="none"
+},2000)
+
+function erroa8() {
+    document.getElementById('a8').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a8').style.display="none"
+},2000)
+
+function errob8() {
+    document.getElementById('b8').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b8').style.display="none"
+},2000)
+
+function erroc8() {
+    document.getElementById('c8').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c8').style.display="none"
+},2000) 
+
+/*questao 9 */
+
+function errod9() {
+    document.getElementById('d9').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d9').style.display="none"
+},2000)
+
+function acertoa9(acerto) {
+    document.getElementById('a9').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('a9').style.display="none"
+},2000)
+
+function errob9() {
+    document.getElementById('b9').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b9').style.display="none"
+},2000)
+
+function erroc9() {
+    document.getElementById('c9').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('c9').style.display="none"
+},2000) 
+
+/*questao 10 */
+
+function errod10() {
+    document.getElementById('d10').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('d10').style.display="none"
+},2000)
+
+function erroa10() {
+    document.getElementById('a10').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('a10').style.display="none"
+},2000)
+
+function errob10() {
+    document.getElementById('b10').style.display="block"
+}
+setInterval(function(){
+    document.getElementById('b10').style.display="none"
+},2000)
+
+function acertoc10(acerto) {
+    document.getElementById('c10').style.display="block"
+    totalAcerto= totalAcerto++
+}
+setInterval(function(){
+    document.getElementById('c10').style.display="none"
+},2000)
+
+if (totalAcerto ==10){
+    alert('show')
+
+}
